eec4a9cae27ab37a4fe0944967bb4952848db8c8
ajinkya3223 <ajinkya.bhale@neosoftmail.com>
2023-03-30 17:32:44 +0530

    commited

--- src/com/poc/mapp/PocOfMap.java
+++ src/com/poc/mapp/PocOfMap.java
@@ -1,9 +1,6 @@
 package com.poc.mapp;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class PocOfMap {
 
@@ -42,7 +39,87 @@ public class PocOfMap {
             System.out.println( entry.getValue());
         }
 
+
+        /** an ArrayList of String object  to map
+         * input list of string
+         * output key value pairs where key is string and value is its length
+         * **/
+        List<String> listOfString = new ArrayList<>();
+        listOfString.add("Java");
+        listOfString.add("JavaScript");
+        listOfString.add("Python");
+        listOfString.add("C++");
+        listOfString.add("Ruby");
+
+        System.out.println("list of string: " + listOfString);
+
+        /** converting ArrayList to HashMap **/
+        Map<String, Integer> map5 = new HashMap<>();
+        for (String str : listOfString) {
+            map5.put(str, str.length());
+        }
+
+
+        Map<String, Integer> numberMapping = new HashMap<>();
+
+        // Adding key-value pairs to a HashMap
+        numberMapping.put("One", 1);
+        numberMapping.put("Two", 2);
+        numberMapping.put("Three", 3);
+
+        /** Add a new key-value pair only if the key does not exist in the HashMap, or is mapped to `null` **/
+        numberMapping.putIfAbsent("Four", 4);
+
+        System.out.println(numberMapping);
+
+
+/** input country and their codes
+ * problem printing hashmaps keyset and values
+ * output india IN**/
+        Map<String, String> countryISOCodeMapping = new HashMap<>();
+
+        countryISOCodeMapping.put("India", "IN");
+        countryISOCodeMapping.put("United States of America", "US");
+        countryISOCodeMapping.put("Russia", "RU");
+        countryISOCodeMapping.put("Japan", "JP");
+        countryISOCodeMapping.put("China", "CN");
+
+        // HashMap's entry set
+        Set<Map.Entry<String, String>> countryISOCodeEntries = countryISOCodeMapping.entrySet();
+        System.out.println("countryISOCode entries : " + countryISOCodeEntries);
+
+        // HashMap's key set
+        Set<String> countries = countryISOCodeMapping.keySet();
+        System.out.println("countries : " + countries);
+
+        // HashMap's values
+        Collection<String> isoCodes = countryISOCodeMapping.values();
+        System.out.println("isoCodes : " + isoCodes);
+
+
+        /** removing keys from hashmap
+         * input chris lisa
+         * output lisa*/
+
+        Map<String, String> mapping = new HashMap<>();
+        mapping.put("Jack", "Marie");
+        mapping.put("Chris", "Lisa");
+        mapping.put("Steve", "Jennifer");
+
+
+
+        // Remove a key from the HashMap
+        // Ex - Unfortunately, Chris got divorced. Let's remove him from the mapping
+        String h = "Chris";
+        String w = mapping.remove(h);
+       System.out.println(w);
+        for(String k: mapping.keySet()){
+
+            System.out.println(k+"  "+mapping.get(k));
+        }
     }
 
-}
+    }
+
+
 
--- src/com/poc/sett/PocOfSet.java
+++ src/com/poc/sett/PocOfSet.java
@@ -17,9 +17,56 @@ public class PocOfSet {
             System.out.println(itr.next());
         }
 
+/** contains method of hashset
+ * input cityname
+ * problem to check weather paris is present or not
+ * output paris   **/
+
+        Set<String> popularCities = new HashSet<>();
+
+        // Check if a HashSet is empty
+        System.out.println("Is popularCities set empty? : " + popularCities.isEmpty());
+
+        popularCities.add("London");
+        popularCities.add("New York");
+        popularCities.add("Paris");
+        popularCities.add("Dubai");
+
+        // Find the size of a HashSet
+        System.out.println("Number of cities in the HashSet " + popularCities.size());
+
+        // Check if the HashSet contains an element
+        String cityName = "Paris";
+        if(popularCities.contains(cityName)) {
+            System.out.println(cityName + " is in the popular cities set.");
+        } else {
+            System.out.println(cityName + " is not in the popular cities set.");
+        }
 
 
+/**  user defined object in set
+ * input two obbject of type customer
+ * output what will happen if we added similar object in set it should have removed duplicate
+ * problem :set will use equals and hashcode to check where object are equals or not
+ */
+
+Set <Customer> cust = new HashSet<Customer>();
+        cust.add(new Customer(101,"Rajeev"));
+        cust.add(new Customer(102,"Sachin"));
+        cust.add(new Customer(103,"Chris"));
+
+        /*
+          HashSet will use the `equals()` & `hashCode()` implementations
+          of the Customer class to check for duplicates and ignore them
+        */
+        cust.add(new Customer(101, "Rajeev"));
+
+        for(Customer c:cust){
+
+            System.out.println(c.getName());
 
     }
 
-}
+}}
+
+
